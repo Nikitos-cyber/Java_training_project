adcf07c1cce7d54bfe42371a30e23b8451b98ffc
ngolomazdin <golomazdin1212@gmail.com>
2026-09-21 13:06:16 +0300

    fix: sout

--- src/Array/Main.java
+++ src/Array/Main.java
@@ -18,8 +18,8 @@ public class Main {
         Arrays.sort(copy);
 
         System.out.printf("Arr: %s", Arrays.toString(arr));
-        System.out.println(String.format("\nCopy: %1$s", Arrays.toString(copy)));
+        System.out.println(String.format("Copy: %1$s", Arrays.toString(copy)));
         System.out.println("Equals: Arr and Copy: " + Arrays.equals(arr,copy));
-        System.out.println("deepEquals: Arr and Copy: " + Arrays.deepEquals(arr,copy));
+        System.out.println("deepEquals: Arr and Copy: ");
     }
 }
